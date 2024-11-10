00f9a9891c885027102843ab21a2977583daa592
John Doe <johndoe@example.com>
2024-11-10 14:18:09 +0100

    updating work

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../assets/logo.png';
 import hero from '../assets/hero.jpeg';
 import video from '../assets/hero.mp4'
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
@@ -16,7 +17,11 @@ const Hero = () => {
     bg-gradient-to-b from-transparent 
     from-70% to-black'></div>
     <div className=' relative z-20 flex h-screen flex-col justify-end pb-20'>
-        <img src={logo} alt="restaura" 
+        <motion.img
+        initial={{opacity:0, y: 50}}
+        animate={{opacity: 1, y: 0}}
+        transition={{duration: 0.5}}
+         src={logo} alt="restaura" 
         className=' w-full p-4' />
         <p className=' p-4 text-lg tracking-tighter text-white'>Paris</p>
     </div>
--- src/component/Mission.jsx
+++ src/component/Mission.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import missionImg from '../assets/mission.jpeg'
 import mission from '../assets/mission.mp4'
 import {MISSION} from '../constants'
+import { motion } from 'framer-motion'
+
 
 const Mission = () => {
   return (
@@ -9,14 +11,31 @@ const Mission = () => {
     <div className=' container mx-auto text-center'>
         <h2 className='mb-8 text-3xl lg:text-4xl'>Our Mission</h2>
         <div className=' relative flex items-center justify-center'>
-            <video src={mission} type="video/mp4" 
-            className=' w-full rounded-3xl' muted autoPlay loop playsInline poster={missionImg}>
+            <motion.video
+                  initial={{opacity: 0}}
+                 whileInView={{opacity: 1}}
+                 viewport={{once: true}}
+                 transition={{duration: 1}}
+            src={mission} type="video/mp4" 
+            className=' w-full rounded-3xl' 
+            muted autoPlay loop playsInline 
+            poster={missionImg}>
 
-            </video>
-            <div className=' absolute h-full w-full rounded-3xl bg-black/40'></div>
-            <p className=' absolute max-w-lg tracking-tighter lg:text-3xl text-white'>
+            </motion.video>
+            <motion.div
+             initial={{opacity: 0}}
+             whileInView={{opacity: 1}}
+             viewport={{once: true}}
+             transition={{duration: 0.5, delay: 0.5}}
+            className=' absolute h-full w-full rounded-3xl bg-black/40'></motion.div>
+            <motion.p
+             initial={{opacity: 0, y: 20}}
+             whileInView={{opacity: 1, y: 0}}
+             viewport={{once: true}}
+             transition={{duration: 1, delay: 0.5}}
+            className=' absolute max-w-lg tracking-tighter lg:text-3xl text-white'>
                 {MISSION}
-            </p>
+            </motion.p>
         </div>
     </div>
  </section>
